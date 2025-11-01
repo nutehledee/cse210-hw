d0ba35989b0737b9d32fbbc96d5fe979db2e127d
Ledee Nuteh Barineka <nutehledee@gmail.com>
2025-11-01 23:49:05 +0100

    week01/Exercise1/Program.cs

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -2,8 +2,17 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        // 1. Ask for first name
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+
+        // 2. Ask for last name
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        // 3. Print the required sentence
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
