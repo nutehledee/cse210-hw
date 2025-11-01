fd19c4bbbeba314335f09a24edab57c4ecde0e2d
Ledee Nuteh Barineka <nutehledee@gmail.com>
2025-11-02 00:48:55 +0100

    week 1 exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -2,8 +2,51 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        string playAgain = "yes";
+        Random randomGenerator = new Random();
+
+        while (playAgain == "yes")
+        {
+            int magicNumber = randomGenerator.Next(1, 101);
+            int guesses = 0;
+            int guess;
+
+            Console.WriteLine($"[DEBUG] Magic number is: {magicNumber}"); // REMOVE LATER
+
+            do
+            {
+                Console.Write("What is your guess? ");
+                string input = Console.ReadLine();
+
+                // PREVENT CRASH
+                if (!int.TryParse(input, out guess))
+                {
+                    Console.WriteLine("Please enter a number!");
+                    continue;
+                }
+
+                guesses++;
+
+                if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+            } while (guess != magicNumber);
+
+            Console.WriteLine($"It took you {guesses} guesses.");
+
+            Console.Write("Do you want to play again? (yes/no) ");
+            playAgain = Console.ReadLine().ToLower();
+        }
     }
 }
